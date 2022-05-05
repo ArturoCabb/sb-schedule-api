da44f15919ad67a557c0bd3542c6543f7c38f210
Arturo Caballero <arturcabbb@outlook.com>
2022-05-04 19:58:32 -0500

    Primer intento
    Primer intento de hacer esto

--- src/main/java/com/at/internship/schedule/Main.java
+++ src/main/java/com/at/internship/schedule/Main.java
@@ -1,11 +1,18 @@
 package com.at.internship.schedule;
 
+import com.at.internship.schedule.config.DefaultRepositoryConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DefaultRepositoryConfig.class);
+        
     }
 }
--- src/main/java/com/at/internship/schedule/config/DefaultRepositoryConfig.java
+++ src/main/java/com/at/internship/schedule/config/DefaultRepositoryConfig.java
@@ -1,7 +1,14 @@
 package com.at.internship.schedule.config;
 
+import com.at.internship.schedule.domain.Appointment;
 import com.at.internship.schedule.repository.IAppointmentRepository;
 import com.at.internship.schedule.repository.IContactRepository;
+import com.at.internship.schedule.repository.impl.CsvAppointmentRepository;
+import com.at.internship.schedule.repository.impl.CsvContactRepository;
+import com.at.internship.schedule.repository.impl.MockAppointmentRepositoryImpl;
+import com.at.internship.schedule.repository.impl.MockContactRepositoryImpl;
+import com.at.internship.schedule.config.MockRepositoryConfig;
+import com.at.internship.schedule.config.CsvRepositoryConfig;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
@@ -9,18 +16,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DefaultRepositoryConfig {
 
+    /**
+     * Al modificar un propertie saber si va a usar el csvRepository o el mockRepository
+     * tanto para contact como appointment
+     */
+
+    private static final String PROP_DEFAULT_REPOSITORY_CONFIG = "${com.at.intership.default.repository.config}";
+
     private final ApplicationContext context;
 
     public DefaultRepositoryConfig(ApplicationContext context) {
         this.context = context;
-        this.context.getBean();
+        //this.context.getBean(name);
     }
     
     public IContactRepository contactRepository() {
         // TODO
+        if (PROP_DEFAULT_REPOSITORY_CONFIG.equalsIgnoreCase("csv"))
+            context.getBean(CsvRepositoryConfig.class);
+        else
+        context.getBean(MockRepositoryConfig.class);
+        return new MockContactRepositoryImpl();
     }
 
     public IAppointmentRepository appointmentRepository() {
-        // TODO
+        if (PROP_DEFAULT_REPOSITORY_CONFIG.equalsIgnoreCase("csv"))
+            context.getBean(CsvRepositoryConfig.class);
+        else
+            context.getBean(MockRepositoryConfig.class);
+        return new MockAppointmentRepositoryImpl();
     }
 }
